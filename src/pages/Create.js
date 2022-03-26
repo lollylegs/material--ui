50c45bcc6291b152a5909d9db6696f3e3dc3999e
MareaNorto <nortonmarea@gmail.com>
2022-03-26 16:48:51 +1030

    finish #5 makeStyles (CSS)

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -2,14 +2,29 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
-import SendIcon from '@material-ui/icons/Send';
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
+import { makeStyles } from '@material-ui/core';
 
+const useStyles = makeStyles({
+  btn: {
+    fontSize: 60,
+    backgroundColor: 'violet',
+    '&:hover': {
+      backgroundColor: 'blue'
+    }
+  },
+  title: {
+    textDecoration: 'underline',
+    marginBottom: 20
+  }
+})
 
 export default function Create() {
+  const classes = useStyles()
   return (
     <Container>
       <Typography
+        className={classes.title}
         variant='h6'
         color="textSecondary"
         component="h2"
@@ -18,6 +33,7 @@ export default function Create() {
         Create a New Note
       </Typography>
       <Button
+        className={classes.btn}
         onClick={() => console.log('you clicked me')}
         type="submit"
         color="secondary"

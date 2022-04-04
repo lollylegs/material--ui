d64069adb92ffce57aeb06be9722f0626544d590
MareaNorto <nortonmarea@gmail.com>
2022-04-04 14:58:20 +0930

    finish #13 Permanent Drawers

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,23 @@
 import { makeStyles } from '@material-ui/core';
 import React from 'react';
+import Drawer from '@material-ui/core/Drawer';
+import Typography from '@material-ui/core/Typography';
+
+const drawerWidth = 240
 
 const useStyles = makeStyles({
    page: {
       background: '#D3D3D3',
       width: '100%'
+   },
+   drawerPaper: {
+      width: drawerWidth
+   },
+   drawer: {
+      width: drawerWidth
+   },
+   root: {
+      display: 'flex'
    }
 })
 
@@ -12,10 +25,24 @@ export default function Layout({ children }) {
    const classes = useStyles()
 
    return (
-      <div>
+      <div className={classes.root}>
          {/* app bar */}
 
          {/* side drawer */}
+         <Drawer
+            className={classes.drawer}
+            variant="permanent"
+            anchor="left"
+            classes={{ paper: classes.drawerPaper }}
+         >
+            <div>
+               <Typography variant="h5">
+                  Ninja Notes
+               </Typography>
+               
+            </div>
+         </Drawer>
+           
 
          <div className={classes.page}>
             {children}
